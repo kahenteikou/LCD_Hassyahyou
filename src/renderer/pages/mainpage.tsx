df4304fe7fb797ee7e8199eccfa5210c3b8a5559
kahenteikou <kokkiemouse@gmail.com>
2022-12-31 11:10:03 +0900

    3

--- src/renderer/pages/mainpage.tsx
+++ src/renderer/pages/mainpage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import LCDContent from "renderer/content/LCDContent";
+import MainPageCSS from "./mainpage.module.css";
+
 const MainPage=()=>{
     return(
-            <div>
-                abcdefg!
-                <LCDContent/>
+            <div id="wrapper">
+                <div id="content">
+                    <LCDContent/>
+                </div>
             </div>
     );
 }
